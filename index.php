7c8ab2bac50fd97f1d98a9297622434b1478f225
damysos34 <damysos.34@gmail.com>
2015-04-19 19:59:27 +0200

    Accueil 19042015

---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+include 'header.php';
+?>
+
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-md-12 main">
+          <h1 class="page-header">Pages</h1>
+
+          <div class="row placeholders">
+
+            <div class="col-sm-2 placeholder">
+              <a href="https://www.google.fr" target="_blank"> 
+              <img src="./style/img/google.png" alt="Google" style="width:100px;height:60px"> 
+              </a>
+              <p class="mytext-muted">Google</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="https://mail.google.com" target="_blank"> 
+               <img src="./style/img/gmail.png" alt="gmail" style="width:100px;height:60px"> 
+              </a>
+              <p class="mytext-muted">Gmail</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="https://translate.google.com/?hl=fr" target="_blank"> 
+              <img src="./style/img/trad.png" alt="Trad" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Trad</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="https://t411.me/" target="_blank"> 
+              <img src="./style/img/t411.png" alt="Trad" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">T411</p>
+            </div>
+
+          </div> <!-- class="row placeholders" -->
+
+          <div class="row placeholders">
+
+              <div class="col-sm-2 placeholder">
+                <a href="http://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal" target="_blank"> 
+                <img src="./style/img/wikipedia.jpg" alt="wikipedia" style="width:100px;height:60px">
+                </a>
+                <p class="mytext-muted">Wikipédia</p>
+              </div>
+
+          </div> <!-- class="row placeholders" -->
+
+          <hr />
+    <div class="titre">Actu </div>
+
+          <div class="row placeholders">
+
+              <div class="col-sm-2 placeholder">
+                <a href="http://www.lemonde.fr/" target="_blank"> 
+                <img src="./style/img/le_monde.png" alt="leMonde" style="width:100px;height:60px">
+                </a>
+                <p class="mytext-muted">Le monde</p>
+              </div>
+
+              <div class="col-sm-2 placeholder">
+                <a href="http://www.vice.com/fr/" target="_blank"> 
+                <img src="./style/img/vice.png" alt="Vice" style="width:100px;height:60px">
+                </a>
+                <p class="mytext-muted">Vice</p>
+              </div>
+
+          </div> <!-- class="row placeholders" -->
+
+          <hr />
+          <div class="titre">Actus info </div>
+
+          <div class="row placeholders">
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://korben.info/" target="_blank"> 
+              <img src="./style/img/korben.png" alt="korben" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Korben</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://www.journaldugeek.com/" target="_blank"> 
+              <img src="./style/img/jdg.jpg" alt="journalDuGeek" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Journal du Geek</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://www.frandroid.com/" target="_blank"> 
+              <img src="./style/img/frandroid.jpg" alt="FrAndroid" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">FrAndroid</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://www.tomsguide.fr/" target="_blank"> 
+              <img src="./style/img/tomsguide.png" alt="Tom'sGuide" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Tom's Guide</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://www.futura-sciences.com/" target="_blank"> 
+              <img src="./style/img/futurasciences.jpg" alt="Futura-Sciences" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Futura-Sciences</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="http://akihabaranews.fr/" target="_blank"> 
+              <img src="./style/img/akihabara.png" alt="AkihabaraNews" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">AkihabaraNewss</p>
+            </div>
+
+            <div class="col-sm-2 placeholder">
+              <a href="https://www.laquadrature.net/fr" target="_blank"> 
+              <img src="./style/img/quadraturedunet.png" alt="quadraturedunet" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">La quadrature du net</p>
+            </div>
+
+          </div> <!-- class="row placeholders" -->
+
+          <hr />
+          <div class="titre">EPSI</div>
+
+          <div class="row placeholders">
+
+            <div class="col-xs-6 col-sm-2 placeholder">
+              <a href="http://ecampusmontpellier.epsi.fr/bienvenue-sur-votre-ecampus" target="_blank"> 
+              <img src="./style/img/ecampus.jpg" alt="Ecampus" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">EPSI Montpellier</p>
+            </div>
+
+            <div class="col-xs-6 col-sm-2 placeholder">
+              <a href="https://extranet.montpellier.epsi.fr/Account/Login" target="_blank"> 
+              <img src="./style/img/ingenium.png" alt="ingénium" style="width:100px;height:60px">
+              </a>
+              <p class="mytext-muted">Ingénium</p>
+            </div>
+
+          </div> <!-- class="row placeholders" -->
+
+        </div> <!--  class="container-fluid" -->
+    </div> <!-- class="row" -->
+</div> <!-- class="col-sm-9 col-md-10 main" -->
+
+<!-- Muscu, diet, Zap, banque, free, edf, shopping sport wiki séries twin  -->
+ 
+<?php
+include 'footer.php';
+?>
