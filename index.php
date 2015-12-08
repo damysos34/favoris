767b5c5901c55f0fd693fbb72b755909080dec08
damysos34 <damysos.34@gmail.com>
2015-12-08 21:11:41 +0100

    V3.0

--- index.php
+++ index.php
@@ -5,376 +5,288 @@ include 'header.php';
 <div class="table-responsive">
   <table width="90%">
 		<tr>
-  			<td>
-          <div class="case">
-            <a href="https://www.google.fr" target="_blank">
-            <img src="./style/img/google.png" alt="Google">
-            </a>
-          </div>
-        </td>
+      			<td>
+              <div class="case">
+                <a href="https://www.google.fr" target="_blank">
+                <img src="./style/img/google.png" alt="Google">
+                </a>
+              </div>
+            </td>
 
-        <td>
-          <div class="case">
-            <a href="https://ixquick.fr/fra/" target="_blank">
-            <img src="./style/img/ixquick.png" alt="reporterre">
-            </a>
-          </div>
-        </td>
+            <td>
+              <div class="case">
+                <a href="https://ixquick.fr/fra/" target="_blank">
+                <img src="./style/img/ixquick.png" alt="reporterre">
+                </a>
+              </div>
+            </td>
 
-        <td>
-            <div class="case">
-            <a href="https://www.facebook.com/" target="_blank">
-            <img src="./style/img/facebook.png" alt="FB">
-            </a>
-          </div>
-        </td>
+            <td>
+                <div class="case">
+                <a href="https://www.facebook.com/" target="_blank">
+                <img src="./style/img/facebook.png" alt="FB">
+                </a>
+              </div>
+            </td>
 
-        <td>
-          <div class="case">
-            <a href="https://www.labanquepostale.fr/" target="_blank">
-            <img src="./style/img/lbp.png" alt="labanquepostale">
-            </a>
-          </div>
-        </td>
+            <td>
+              <div class="case">
+                <a href="https://www.labanquepostale.fr/" target="_blank">
+                <img src="./style/img/lbp.png" alt="labanquepostale">
+                </a>
+              </div>
+            </td>
 
-        <td>
-          <div class="case">
-            <a href="http://www.lequipe.fr/" target="_blank">
-            <img src="./style/img/lequipe.jpg" alt="lequipe">
-            </a>
-          </div>
-        </td>
+            <td>
+              <div class="case">
+                <a href="http://www.lequipe.fr/" target="_blank">
+                <img src="./style/img/lequipe.jpg" alt="lequipe">
+                </a>
+              </div>
+            </td>
 
+            <td>
+                <div class="case">
+                <a href="http://www.meteofrance.com/accueil" target="_blank">
+                <img src="./style/img/meteofrance.jpg" alt="meteo">
+                </a>
+              </div>
+            </td>
 
-      <td></td>
+            <td>
+              <div class="case">
+                <a href="http://www.realstreamunited.com/football-live-streaming-video.html" target="_blank">
+                <img src="./style/img/streamhunter.png" alt="streamhunter">
+                </a>
+              </div>
+            </td>
 
-      <td>
-        <div class="case">
-          <a href="http://ecampusmontpellier.epsi.fr/bienvenue-sur-votre-ecampus" target="_blank">
-          <img src="./style/img/ecampus.jpg" alt="Ecampus">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="https://extranet.montpellier.epsi.fr/Account/Login" target="_blank">
-          <img src="./style/img/ingenium.png" alt="ingénium">
-          </a>
-        </div>
-      </td>
-
-      </tr> <!-- Première ligne -->
-
-      <tr>
-
-        <td>
-        <div class="case">
-          <a href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal" target="_blank">
-          <img src="./style/img/wikipedia.jpg" alt="wikipedia">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="https://t411.me/" target="_blank">
-          <img src="./style/img/t411.png" alt="Trad">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="http://78.194.14.113:2822/" target="_blank">
-          <img src="./style/img/seedbox.png" alt="freebox">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="http://www.pogdesign.co.uk/cat/" target="_blank">
-          <img src="./style/img/pogdesign.png" alt="pogdesign">
-          </a>
-        </div>
-      </td>
-
-      <td>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="http://slider.kz/" target="_blank">
-          <img src="./style/img/sliderz.jpg" alt="sliderz">
-          </a>
-        </div>
-      </td>
-
-      <td>
-      </td>
-
-      <td>
-      </td>
-
-      </tr>
+    </tr>
 
     <tr>
 
-    <td>
-      <div class="case">
-        <a href="https://www.lemonde.fr/" target="_blank">
-        <img src="./style/img/lemonde.jpg" alt="leMonde">
-        </a>
-      </div>
-    </td>
+            <td>
+              <div class="case">
+                <a href="https://news.google.com/" target="_blank">
+                <img src="./style/img/googlenews.jpg" alt="googlenews">
+                </a>
+              </div>
+            </td>
 
-    <td>
-      <div class="case">
-        <a href="http://www.amnesty.fr/" target="_blank">
-        <img src="./style/img/amnesty.jpg" alt="Amnesty">
-        </a>
-      </div>
-    </td>
+            <td>
+              <div class="case">
+                <a href="https://www.lemonde.fr/" target="_blank">
+                <img src="./style/img/lemonde.jpg" alt="leMonde">
+                </a>
+              </div>
+            </td>
 
-    <td>
-      <div class="case">
-        <a href="http://www.reporterre.net/" target="_blank">
-        <img src="./style/img/reporterre.jpg" alt="reporterre">
-        </a>
-      </div>
-    </td>
+            <td>
+            <div class="case">
+              <a href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal" target="_blank">
+              <img src="./style/img/wikipedia.jpg" alt="wikipedia">
+              </a>
+            </div>
+          </td>
 
-    <td>
-      <div class="case">
-        <a href="http://www.vice.com/fr/" target="_blank">
-        <img src="./style/img/vice.jpg" alt="Vice">
-        </a>
-      </div>
-    </td>
+            <td>
+              <div class="case">
+                <a href="http://www.vice.com/fr/" target="_blank">
+                <img src="./style/img/vice.jpg" alt="Vice">
+                </a>
+              </div>
+            </td>
 
-    <td>
-      <div class="case">
-        <a href="https://news.google.com/" target="_blank">
-        <img src="./style/img/googlenews.jpg" alt="googlenews">
-        </a>
-      </div>
-    </td>
+            <td>
+                <div class="case">
+                <a href="http://izismile.com/" target="_blank">
+                <img src="./style/img/izismile.jpg" alt="izismile">
+                </a>
+              </div>
+            </td>
 
-    <td>
-    </td>
+            <td>
+              <div class="case">
+                <a href="http://www.spi0n.com/zapping-web/" target="_blank">
+                <img src="./style/img/spion.jpg" alt="spion">
+                </a>
+              </div>
+            </td>
 
-    <td>
-      <div class="case">
-        <a href="http://www.futura-sciences.com/" target="_blank">
-        <img src="./style/img/futurasciences.jpg" alt="Futura-Sciences">
-        </a>
-      </div>
-    </td>
-
-    <td>
-      <div class="case">
-        <a href="http://www.nextinpact.com/" target="_blank">
-        <img src="./style/img/nextinpact.png" alt="next-inpact">
-        </a>
-      </div>
-    </td>
+            <td>
+              <div class="case">
+                <a href="http://www.zap-tele.com/" target="_blank">
+                <img src="./style/img/zaptele.jpg" alt="zaptele">
+                </a>
+              </div>
+            </td>
 
 		</tr>
 
     <tr>
 
-    <td>
-      <div class="case">
-        <a href="http://www.senscritique.com/" target="_blank">
-        <img src="./style/img/senscritique.png" alt="senscritique">
-        </a>
-      </div>
-    </td>
+            <td>
+              <div class="case">
+                <a href="http://www.senscritique.com/" target="_blank">
+                <img src="./style/img/senscritique.png" alt="senscritique">
+                </a>
+              </div>
+            </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.realstreamunited.com/football-live-streaming-video.html" target="_blank">
-          <img src="./style/img/streamhunter.png" alt="streamhunter">
-          </a>
-        </div>
-      </td>
+            <td>
+              <div class="case">
+                <a href="https://t411.in/" target="_blank">
+                <img src="./style/img/t411.png" alt="Trad">
+                </a>
+              </div>
+            </td>
 
-      <td>
-          <div class="case">
-          <a href="http://www.meteofrance.com/accueil" target="_blank">
-          <img src="./style/img/meteofrance.jpg" alt="meteo">
-          </a>
-        </div>
-      </td>
+            <td>
+              <div class="case">
+                <a href="http://78.194.14.113:2822/" target="_blank">
+                <img src="./style/img/seedbox.png" alt="freebox">
+                </a>
+              </div>
+            </td>
 
-      <td>
-      </td>
+            <td>
+              <div class="case">
+                <a href="http://www.pogdesign.co.uk/cat/" target="_blank">
+                <img src="./style/img/pogdesign.png" alt="pogdesign">
+                </a>
+              </div>
+            </td>
 
-      <td>
-      </td>
+            <td>
+              <div class="case">
+                <a href="http://slider.kz/" target="_blank">
+                <img src="./style/img/sliderz.jpg" alt="sliderz">
+                </a>
+              </div>
+            </td>
 
-      <td>
-          <div class="case">
-          <a href="http://izismile.com/" target="_blank">
-          <img src="./style/img/izismile.jpg" alt="izismile">
-          </a>
-        </div>
-      </td>
+            <td>
+            </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.spi0n.com/zapping-web/" target="_blank">
-          <img src="./style/img/spion.jpg" alt="spion">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="http://www.zap-tele.com/" target="_blank">
-          <img src="./style/img/zaptele.jpg" alt="zaptele">
-          </a>
-        </div>
-      </td>
+            <td>
+                <div class="case">
+                <a href="http://curvefever.com/play2.php" target="_blank">
+                <img src="./style/img/curve.jpg" alt="achtung">
+                </a>
+              </div>
+            </td>
 
     </tr>
 
     <tr>
 
-      <td>
-        <div class="case">
-          <a href="http://korben.info/" target="_blank">
-          <img src="./style/img/korben.png" alt="korben"
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://korben.info/" target="_blank">
+              <img src="./style/img/korben.png" alt="korben"
+              </a>
+            </div>
+          </td>
 
-      <td>
-        <div class="case">
-        <a href="http://www.journaldugeek.com/" target="_blank">
-        <img src="./style/img/jdg.jpg" alt="journalDuGeek">
-        </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.nextinpact.com/" target="_blank">
+              <img src="./style/img/nextinpact.png" alt="next-inpact">
+              </a>
+            </div>
+          </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.frandroid.com/" target="_blank">
-          <img src="./style/img/frandroid.jpg" alt="FrAndroid">
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.frandroid.com/" target="_blank">
+              <img src="./style/img/frandroid.jpg" alt="FrAndroid">
+              </a>
+            </div>
+          </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.tomsguide.fr/" target="_blank">
-          <img src="./style/img/tomsguide.png" alt="Tom'sGuide">
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.tomsguide.fr/" target="_blank">
+              <img src="./style/img/tomsguide.png" alt="Tom'sGuide">
+              </a>
+            </div>
+          </td>
 
-      <td>
-        <div class="case">
-          <a href="https://www.laquadrature.net/fr" target="_blank">
-          <img src="./style/img/quadraturedunet.png" alt="quadraturedunet">
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.infoq.com/fr/" target="_blank">
+              <img src="./style/img/logo_bigger.jpg" alt="infoq"
+              </a>
+            </div>
+          </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.cowcotland.com/" target="_blank">
-          <img src="./style/img/cowcotland.png" alt="cowcotland">
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.lemondeinformatique.fr/" target="_blank">
+              <img src="./style/img/lemondeinfor.jpg" alt="lmd"
+              </a>
+            </div>
+          </td>
 
-      <td>
-      </td>
-
-      <td>
-          <div class="case">
-          <a href="http://curvefever.com/play2.php" target="_blank">
-          <img src="./style/img/curve.jpg" alt="achtung">
-          </a>
-        </div>
-      </td>
+          <td>
+            <div class="case">
+              <a href="http://www.begeek.fr/" target="_blank">
+              <img src="./style/img/begeek.jpg" alt="begeek"
+              </a>
+            </div>
+          </td>
 
     </tr>
 
     <tr>
 
-      <td>
-        <div class="case">
-          <a href="https://linuxfr.org/" target="_blank">
-          <img src="./style/img/linuxfr.png" alt="linuxfr">
-          </a>
-        </div>
-      </td>
+        <td>
+          <div class="case">
+            <a href="http://ecampusmontpellier.epsi.fr/bienvenue-sur-votre-ecampus" target="_blank">
+            <img src="./style/img/ecampus.jpg" alt="Ecampus">
+            </a>
+          </div>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="http://memo-linux.com/" target="_blank">
-          <img src="./style/img/MEMONUX.png" alt="memo-linux">
-          </a>
-        </div>
-      </td>
+        <td>
+          <div class="case">
+            <a href="https://extranet.montpellier.epsi.fr/Account/Login" target="_blank">
+            <img src="./style/img/ingenium.png" alt="ingénium">
+            </a>
+          </div>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.undernews.fr/" target="_blank">
-          <img src="./style/img/undernews.png" alt="undernews">
-          </a>
-        </div>
-      </td>
+        <td>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.zdnet.fr/" target="_blank">
-          <img src="./style/img/zdnet.jpg" alt="zdnet">
-          </a>
-        </div>
-      </td>
+        <td>
+          <div class="case">
+            <a href="http://www.futura-sciences.com/" target="_blank">
+            <img src="./style/img/futurasciences.jpg" alt="Futura-Sciences">
+            </a>
+          </div>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="http://www.datasecuritybreach.fr/" target="_blank">
-          <img src="./style/img/databreach.jpeg" alt="databreach">
-          </a>
-        </div>
-      </td>
+        <td>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="https://thehackernews.com/" target="_blank">
-          <img src="./style/img/hackernews.jpg" alt="hackernews">
-          </a>
-        </div>
-      </td>
+        <td>
+          <div class="case">
+            <a href="http://www.cowcotland.com/" target="_blank">
+            <img src="./style/img/cowcotland.png" alt="cowcotland">
+            </a>
+          </div>
+        </td>
 
-      <td>
-        <div class="case">
-          <a href="http://blog.seboss666.info/" target="_blank">
-          <img src="./style/img/BLOGSEB.png" alt="seboss666">
-          </a>
-        </div>
-      </td>
-
-      <td>
-        <div class="case">
-          <a href="https://infos.mytux.fr/" target="_blank">
-          <img src="./style/img/journalhacker.png" alt="hackerjournal">
-          </a>
-        </div>
-      </td>
+        <td>
+          <div class="case">
+            <a href="https://infos.mytux.fr/" target="_blank">
+            <img src="./style/img/journalhacker.png" alt="hackerjournal">
+            </a>
+          </div>
+        </td>
 
     </tr>
-
-
-
-
-
 	</table>
 </div>
 
